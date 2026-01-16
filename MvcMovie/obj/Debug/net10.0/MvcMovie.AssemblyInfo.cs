925a9d4b74f3fec6a40792adaa7e87ea2f3a09d8
gatlingmcgee <rzman03@aol.com>
2026-01-16 16:52:48 -0600

    added movies

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ff828013ab05a5eebe96f22f58628b483ebda7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4efcd4781e96c4554c2fc494f97260dada8c72f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
